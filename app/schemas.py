aa068cc8e234da1883701c8d48544bb40c41bf97
athendd <141829395+athendd@users.noreply.github.com>
2026-03-10 13:09:53 -0400

    added docstrings to schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -6,6 +6,9 @@ from typing import Generic, List, TypeVar
 
 T = TypeVar('T')
 class PaginatedResponse(BaseModel, Generic[T]):
+    """
+    Generic schema used for paginated API response
+    """
     items: List[T]
     total: int
     page: int
@@ -14,22 +17,28 @@ class PaginatedResponse(BaseModel, Generic[T]):
     has_next: bool
     has_prev: bool
 
-#Schema for creating an item
 class ItemCreate(BaseModel):
+    """
+    Schema used for created item requests
+    """
     title: str
     author: str
     notes: str | None = None
     read: bool
 
-#Schema for updating an item
 class ItemUpdate(BaseModel):
+    """
+    Schema used for updated item requests
+    """
     title: str | None = None
     author: str | None = None
     notes: str | None = None
     read: bool | None = None
 
-#Schema for returning an item
 class ItemResponse(BaseModel):
+    """
+    Schema used for API responses
+    """
     class Config:
         orm_mode = True
     id: int
