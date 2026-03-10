b0fed64a5245667f1dde0e44dc157ccca4357563
athendd <141829395+athendd@users.noreply.github.com>
2026-03-10 13:03:25 -0400

    adding docstrings to items.py

--- app/routes/items.py
+++ app/routes/items.py
@@ -1,4 +1,6 @@
-#Defines the API routes related to items
+"""
+API routes for managing reading list items. 
+"""
 from services.item_service import get_item_service, update_item_service, delete_item_service, create_item_service, get_all_items_service
 from fastapi import APIRouter, Depends, Query
 from database import get_db
@@ -10,20 +12,36 @@ router = APIRouter(prefix = '/items')
 
 @router.get('/{item_id}', response_model = ItemResponse)
 def get_item(item_id: int, db: Session = Depends(get_db)):
+    """
+    API endpoint to retrieve a reading item by ID
+    """
+
     return get_item_service(db, item_id)
 
 @router.put('/{item_id}', response_model = ItemResponse)
 def update_item(item_id: int, updated_item: ItemUpdate, db: Session = Depends(get_db)):
+    """
+    API endpoint to update item by ID
+    """
     return update_item_service(db, item_id, updated_item)
 
 @router.delete('/{item_id}')
 def delete_item(item_id: int, db: Session = Depends(get_db)):
+    """
+    API endpoint to delete item by ID
+    """
     delete_item_service(db, item_id)
 
 @router.post('/', response_model = ItemResponse)
 def create_item(item: ItemCreate, db: Session = Depends(get_db)):
+    """
+    API endpoint to create a new reading item
+    """
     return create_item_service(db, item)
 
 @router.get('/', response_model = Union[List[ItemResponse], PaginatedResponse[ItemResponse]])
 def get_all_items(page: int = Query(None, ge =1), page_size: int = Query(None, ge =1, le = 100, description = "Items per page"), db: Session = Depends(get_db)):
+    """
+    API endpoint to get all items in database
+    """
     return get_all_items_service(db, page, page_size)
